9b723affdf71b3056ef1d6a67895dff32db3392b
Erhansa Pratama <erhansapratama5@gmail.com>
2026-04-05 00:15:26 +0700

    first commit

---
+++ cypress/e2e/Test/Test_sanber.cy.js
@@ -0,0 +1,9 @@
+describe('Scenario verifikasi login', () => {
+    it('TC-Login-001 Login dengan email dan password yang benar', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+        cy.url().should('include', '/inventory.html')
+    })
+})
